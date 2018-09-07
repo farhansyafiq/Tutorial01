3b9c06aedefd677b7e4154f6660321570452907b
Farhan Syafiq <tm.farhansyafiq@gmail.com>
2018-09-07 14:24:14 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,25 @@
+
+public class Dosen {
+	private String nama;
+	private String matkul;
+	public Dosen(String nama, String matkul) {
+		super();
+		this.nama = nama;
+		this.matkul = matkul;
+	}
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getMatkul() {
+		return matkul;
+	}
+	public void setMatkul(String matkul) {
+		this.matkul = matkul;
+	}
+	
+	
+}
